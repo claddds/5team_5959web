610842424cf8e41bccd09d5be0c6629422c9e0c6
hayeongJ <hayoungju98@naver.com>
2023-10-02 19:30:44 +0900

    1002 QNA 최종

--- oguogu/src/main/java/com/oguogu/qna/controller/Qna_Controller.java
+++ oguogu/src/main/java/com/oguogu/qna/controller/Qna_Controller.java
@@ -49,16 +49,16 @@ public class Qna_Controller {
 	@Autowired
 	private Paging paging;
 	
-	// qna는 유저의 1:1 문의 게시판
-	@RequestMapping("/qna_list.do")
+	// qna�뒗 �쑀���쓽 1:1 臾몄쓽 寃뚯떆�뙋
+	@RequestMapping("/userqna_list.do")
 	public ModelAndView qnaList(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView("qna/qna_list");
-		// 페이징
-		// 전체 게시물의 수
+		// �럹�씠吏�
+		// �쟾泥� 寃뚯떆臾쇱쓽 �닔
 		int count = qna_Service.getTotalCount();
 		paging.setTotalRecord(count);
 
-		// 전체 페이지의 수
+		// �쟾泥� �럹�씠吏��쓽 �닔
 		if (paging.getTotalRecord() <= paging.getNumPerPage()) {
 			paging.setTotalPage(1);
 		} else {
@@ -67,7 +67,7 @@ public class Qna_Controller {
 				paging.setTotalPage(paging.getTotalPage() + 1);
 			}
 		}
-		// 현재 페이지
+		// �쁽�옱 �럹�씠吏�
 		String cPage = request.getParameter("cPage");
 		if (cPage == null) {
 			paging.setNowPage(1);
@@ -76,13 +76,13 @@ public class Qna_Controller {
 		}
 		paging.setOffset(paging.getNumPerPage() * (paging.getNowPage() - 1));
 
-		// 시작블록과 끝블록 구하기
+		// �떆�옉釉붾줉怨� �걹釉붾줉 援ы븯湲�
 		paging.setBeginBlock(
 				(int) ((paging.getNowPage() - 1) / paging.getPagePerBlock()) * paging.getPagePerBlock() + 1);
 
 		paging.setEndBlock(paging.getBeginBlock() + paging.getPagePerBlock() - 1);
 
-		// 주의사항
+		// 二쇱쓽�궗�빆
 		if (paging.getEndBlock() > paging.getTotalPage()) {
 			paging.setEndBlock(paging.getTotalPage());
 		}
@@ -108,18 +108,18 @@ public class Qna_Controller {
 			if (file.isEmpty()) {
 				qvo.setOne_fname("");
 			} else {
-				// 같은 이름의 파일 없도록 UUID 사용
+				// 媛숈� �씠由꾩쓽 �뙆�씪 �뾾�룄濡� UUID �궗�슜
 				UUID uuid = UUID.randomUUID();
 				String one_fname = uuid.toString() + "_" + qvo.getFile().getOriginalFilename();
 				qvo.setOne_fname(one_fname);
-				// 이미지 저장
+				// �씠誘몄� ���옣
 				byte[] in = qvo.getFile().getBytes();
 				File out = new File(path, one_fname);
 				FileCopyUtils.copy(in, out);
 			}
-			// 패스워드 암호화
+			// �뙣�뒪�썙�뱶 �븫�샇�솕
 			qvo.setOne_pwd(passwordEncoder.encode(qvo.getOne_pwd()));
-			// DB에 저장하기
+			// DB�뿉 ���옣�븯湲�
 			int res = qna_Service.getInsert(qvo);
 			if (res > 0) {
 				return mv;
@@ -131,7 +131,7 @@ public class Qna_Controller {
 			return null;
 		}
 }
-	// 상세보기
+	// �긽�꽭蹂닿린
 		@GetMapping("/qna_onelist.do")
 		public String qnaOneList(HttpServletRequest request, HttpSession session, RedirectAttributes rttr,Model model) {
 			session.removeAttribute("qna_onelist");
@@ -139,16 +139,16 @@ public class Qna_Controller {
 			String cPage = request.getParameter("cPage");
 			String user_id = (String) session.getAttribute("user_id");
 			
-			// 상세보기
+			// �긽�꽭蹂닿린
 			Qna_VO qvo = qna_Service.getOneList(one_idx);
 			User_VO uvo = join_Service.getUserOneList(user_id);
 			
-			// 댓글 가져오기
+			// �뙎湲� 媛��졇�삤湲�
 			List<QnAComment_VO> c_list = qnaComment_Service.getQnACommList(one_idx);
 			
-			System.out.println("로그인 한 아이디 : " + user_id);
+			System.out.println("濡쒓렇�씤 �븳 �븘�씠�뵒 : " + user_id);
 			String db_id = qvo.getUser_id();
-			System.out.println("디비에 저장된 아이디 : " +db_id);
+			System.out.println("�뵒鍮꾩뿉 ���옣�맂 �븘�씠�뵒 : " +db_id);
 		
 			model.addAttribute("c_list", c_list);
 			model.addAttribute("qvo", qvo);
@@ -165,7 +165,7 @@ public class Qna_Controller {
 	         }
 		}	
 		
-		// 수정
+		// �닔�젙
 		@PostMapping("/qna_updateForm.do")
 		public ModelAndView qnaUpdateForm(@ModelAttribute("cPate") String cPage, @ModelAttribute("one_idx") String one_idx) {
 			ModelAndView mv = new ModelAndView("qna/qna_update");
@@ -174,7 +174,7 @@ public class Qna_Controller {
 			return mv;
 		}
 		
-		// 게시글 수정 등록
+		// 寃뚯떆湲� �닔�젙 �벑濡�
 	    @PostMapping("/qna_update.do")
 	    public ModelAndView getUpdate(Qna_VO qvo, HttpServletRequest request) {
 	        ModelAndView mv = new ModelAndView("qna/");
@@ -202,7 +202,7 @@ public class Qna_Controller {
 	            return null;
 	        }
 	    }
-		// 삭제
+		// �궘�젣
 		@PostMapping("/qna_deleteForm.do")
 		public ModelAndView deleteForm(@ModelAttribute("cPage") String cPage, @ModelAttribute("one_idx") String one_idx) {
 			ModelAndView mv = new ModelAndView("qna/qna_delete");
@@ -212,17 +212,17 @@ public class Qna_Controller {
 		@PostMapping("/qna_delete.do")
 		public String qnaDelete(@RequestParam("one_pwd") String one_pwd, @RequestParam("cPage") String cPage, RedirectAttributes rttr,
 				@ModelAttribute("one_idx") String one_idx, Model model) {
-			// 비밀번호가 맞는지 체크 하자.
-			// DB에서 암호 얻기
+			// 鍮꾨�踰덊샇媛� 留욌뒗吏� 泥댄겕 �븯�옄.
+			// DB�뿉�꽌 �븫�샇 �뼸湲�
 			Qna_VO qvo = qna_Service.getOneList(one_idx);
 			String dbpwd = qvo.getOne_pwd();
 			System.out.println(one_pwd);
-			// passwordEncoder.matches(암호화되지 않은것, 암호화 된것 )
+			// passwordEncoder.matches(�븫�샇�솕�릺吏� �븡��寃�, �븫�샇�솕 �맂寃� )
 			if (!passwordEncoder.matches(one_pwd, dbpwd)) {
 				model.addAttribute("pwchk", "fail");  
 				return "qna/qna_delete";
 			} else {
-				// 원글삭제 시 상태값을 0 => 1 로 변경 시키자
+				// �썝湲��궘�젣 �떆 �긽�깭媛믪쓣 0 => 1 濡� 蹂�寃� �떆�궎�옄
 				int result = qna_Service.getDelete(one_idx);
 				rttr.addFlashAttribute("cPage", cPage);
 				rttr.addAttribute("del_alert","ok");
